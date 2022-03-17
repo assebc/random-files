589694641b65a9797c3a47d5ee6019e1009807bc
assebc <claudioafbessa@gmail.com>
2022-03-17 16:32:20 +0000

    starting java programs, help from ex5 in ficha02.java in uminho-lei/ repos

---
+++ java/Procura.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Procura{
+
+	
+
+	public static void main(String [] args){
+
+		// Swap
+		// Ordena
+		// isElemOfArray
+		// IndexOfElem
+		// SemRepetidos
+		// Repetidos
+	}
+}
