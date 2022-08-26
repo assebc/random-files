e3818d90854131f058a60a69b913c339a2603941
assebc <claudioafbessa@gmail.com>
2022-08-26 02:14:47 +0100

    :wrench: all cases fixed using xor

--- javascript/howold.js
+++ javascript/howold.js
@@ -2,7 +2,7 @@ function diff_years(dt2, dt1){
 
     var diff =(dt2.getTime() - dt1.getTime()) / 1000;
     diff /= (60 * 60 * 24);
-    if(dt2.getUTCDay<dt1.getUTCDay&&dt2.getUTCMonth<=dt1.getUTCMonth) return Math.abs(Math.round(diff/365.25)-1);
+    if((dt2.getUTCMonth() == dt1.getUTCMonth() ^ dt2.getUTCDay() < dt1.getUTCDay()) || (dt2.getUTCMonth() < dt1.getUTCMonth())) return Math.abs(Math.round(diff/365.25)-1);
 
     return Math.abs(Math.round(diff/365.25));
 }
