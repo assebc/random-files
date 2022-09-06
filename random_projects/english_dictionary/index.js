c1f487a937e17e6a4b32ecb861390404e4ee4621
assebc <claudioafbessa@gmail.com>
2022-09-06 21:34:12 +0100

    :heavy_plus_sign: starting making an english dictionary

---
+++ random_projects/english_dictionary/index.js
@@ -0,0 +1,11 @@
+const wrapper = document.querySelector(".wrapper"),
+    searchInput = wrapper.querySelector("input"),
+    volume = wrapper.querySelector(".word i"),
+    infoText = wrapper.querySelector(".info_text"),
+    synonyms = wrapper.querySelector(".synonyms .list"),
+    removeIcon = wrapper.querySelector(".search span");
+    let audio;
+
+function data(result, word){
+    if(result.title){}
+}
