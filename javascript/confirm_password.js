24cd054e97b87eb229b75f21872bb6eacbaed90a
assebc <claudioafbessa@gmail.com>
2022-09-16 13:59:35 +0100

    :wrench: working js

--- javascript/confirm_password.js
+++ javascript/confirm_password.js
@@ -1,9 +1,9 @@
 function onChange() {
-    const password = document.querySelector("input[name=password]");
-    const confirm = document.querySelector("input[name=confirm]");
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=confirm]');
     if (confirm.value === password.value) {
-        confirm.setCustomValidity("");
+      confirm.setCustomValidity('');
     } else {
-        confirm.setCustomValidity("Passwords do not match!");
+      confirm.setCustomValidity('Passwords do not match');
     }
-}
+  }
