36182287a6bd8c96735bcd3c5fcd188235f44695
assebc <claudioafbessa@gmail.com>
2022-11-30 12:47:43 +0000

    :heavy_plus_sign: health markers

---
+++ prolog/health_markers/db/marcador.pl
@@ -0,0 +1,6 @@
+%marcador(IdMarcador,Analise,Age,Gender,Minimum,Maximum)
+marcador(ctm01,colesterol,[18,30],masculino,[0,170]).
+marcador(ctf02,colesterol,[18,30],feminino,[0,160]).
+marcador(ctm03,colesterol,[31,45],masculino,[0,190]).
+marcador(ctf04,colesterol,[31,45],feminino,[0,180]).
+marcador(psm05,pulsacao,[18,25],masculino,[60,80]).
---
+++ prolog/health_markers/db/utente.pl
@@ -0,0 +1,4 @@
+% utente(IdUtente,UtenteName,DataNascimento,Gender)
+utente(123456780,antonio,30/06/1990,masculino).
+utente(987654321,beatriz,30/07/1985,feminino).
+utente(135246978,carlos,30/08/1987,masculino).
