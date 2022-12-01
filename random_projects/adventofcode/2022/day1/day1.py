0923707fed393c06c5f9c6f0fae4f344ed9d6f52
assebc <claudioafbessa@gmail.com>
2022-12-01 20:58:57 +0000

    :heavy_plus_sign: adding part2 of day1 question

--- random_projects/adventofcode/2022/day1/day1.py
+++ random_projects/adventofcode/2022/day1/day1.py
@@ -1,14 +1,33 @@
 file = open("input.txt")
 
-amount = 0
 atm = 0
+top_3 = [0,0,0]
+
+def get_min(top_3=[]):
+    minas = top_3[0]
+    if minas <= top_3[1] and minas <= top_3[2]:
+        return (minas,0)
+    elif minas <= top_3[1]:
+        return (top_3[2],2)
+    elif minas>= top_3[1] and top_3[1]>= top_3[2]:
+        return (top_3[2],2)
+    else:
+        return (top_3[1],1)
 
 for line in file.readlines():
     if str(line[0]) != '\n':
         atm += int(line)
     else:
-        if amount < atm:
-            amount = atm
+        (mina,pos) = get_min(top_3)
+        if atm > mina:
+            top_3[pos] = atm
         atm = 0
 
+amount = 0
+for i in top_3:
+    amount += i
+print(top_3)
 print(amount)
+
+
+        
